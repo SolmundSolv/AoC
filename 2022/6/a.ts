582592a32134fdec735936d927e51503289d2252
SolmundSolv <konradqxd@gmail.com>
2022-12-06 14:00:58 +0100

    Day 6

--- 2022/6/a.ts
+++ 2022/6/a.ts
@@ -5,26 +5,8 @@ const test2 = "nppdvjthqldpwncqszvftbrmjlhg";
 const test3 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
 const test4 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
 
-let lastFourLetters = init(input, 4);
-let lastFourteenLetters = init(input, 14);
-
-for (let i = 4; i < input.length; i++) {
-    if (!hasRepeats(lastFourLetters)) {
-        console.log("1. " + i);
-        break;
-    }
-    //slice first letter and add the new one to the end
-    lastFourLetters = lastFourLetters.slice(1) + input[i];
-}
-
-for (let i = 14; i < input.length; i++) {
-    if (!hasRepeats(lastFourteenLetters)) {
-        console.log("2. " + i);
-        break;
-    }
-    //slice first letter and add the new one to the end
-    lastFourteenLetters = lastFourteenLetters.slice(1) + input[i];
-}
+console.log("1. " + testing(input, 4));
+console.log("2. " + testing(input, 14));
 
 function hasRepeats(str: string) {
     return /(.).*\1/.test(str);
@@ -33,4 +15,15 @@ function hasRepeats(str: string) {
 function init(str: string, length: number) {
     //first 14 letters
     return str.slice(0, length);
+}
+
+function testing(input: string, length: number) {
+    let lastLetters = init(input, length);
+    for (let i = length; i < input.length; i++) {
+        if (!hasRepeats(lastLetters)) {
+            return i;
+        }
+        //slice first letter and add the new one to the end
+        lastLetters = lastLetters.slice(1) + input[i];
+    }
 }
